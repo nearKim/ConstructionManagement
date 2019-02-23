075b62a10a3f9e29eda2900d94eca90b871c8d71
nearKim <garfield@snu.ac.kr>
2019-02-23 14:58:44 +0900

    Add  management app

--- managements/models.py
+++ managements/models.py
@@ -5,12 +5,12 @@ from ConstructionManagement.mixins import TimeStampedMixin, CaculatedMixin
 
 
 class Project(TimeStampedMixin):
-    name = models.CharField(_('프로젝트 이름'))
+    name = models.CharField( _('프로젝트 이름'), max_length=300)
     description = models.TextField(_('프로젝트 설명'), blank=True)
 
 
 class Activity(TimeStampedMixin):
-    name = models.CharField(_('액티비티 이름'))
+    name = models.CharField(_('액티비티 이름'), max_length=300)
     description = models.TextField(_('액티비티 설명'), blank=True)
     duration = models.PositiveIntegerField(_('기간'), default=0)
     productivity = models.FloatField(_('생산성'), default=0.0)
@@ -18,24 +18,24 @@ class Activity(TimeStampedMixin):
 
     project = models.ForeignKey('managements.Project', related_name='activities', on_delete=models.CASCADE)
     resource = models.ForeignKey('managements.Resource', related_name='activities', on_delete=models.PROTECT)
-    work_package = models.ManyToManyField('managements.models.WorkPackage', related_name='activities')
+    work_package = models.ManyToManyField('managements.WorkPackage', related_name='activities')
 
 
 class Resource(TimeStampedMixin):
-    name = models.CharField(_('리소스 이름'))
+    name = models.CharField(_('리소스 이름'), max_length=300)
     productivity = models.FloatField(_('생산성'), default=0.0)
-    work_package = models.ManyToManyField('managements.models.WorkPackage', related_name='resources')
+    work_package = models.ManyToManyField('managements.WorkPackage', related_name='resources')
 
 
 class Duration(CaculatedMixin):
     description = models.TextField(_('기간 설명'), blank=True)
-    work_package = models.ManyToManyField('managements.models.WorkPackage', related_name='durations')
+    work_package = models.ManyToManyField('managements.WorkPackage', related_name='durations')
 
 
 class Productivity(CaculatedMixin):
     description = models.TextField(_('생산성 설명'), blank=True)
-    work_package = models.ManyToManyField('managements.models.WorkPackage', related_name='productivities')
+    work_package = models.ManyToManyField('managements.WorkPackage', related_name='productivities')
 
 
 class WorkPackage(TimeStampedMixin):
-    package = models.CharField(_('Work Package'))
+    package = models.CharField(_('Work Package'), max_length=300)
