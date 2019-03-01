bc003052f6bbcf06c5793d8b6a80ca5d5b69d751
nearKim <garfield@snu.ac.kr>
2019-03-01 15:07:58 +0900

    DurationInfo API 추가함

--- ConstructionManagement/urls.py
+++ ConstructionManagement/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('managements.urls'))
+    path('api/v1/', include('managements.urls')),
+    path('api/v1/', include('informations.urls')),
 ]
--- informations/views.py
+++ informations/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from informations.models import DurationInfo, ProductivityInfo
+from informations.serializers import DurationInfoSerializer
+
+
+class DurationInfoViewSet(viewsets.ModelViewSet):
+    serializer_class = DurationInfoSerializer
+    queryset = DurationInfo.objects.select_related('datainfo_ptr').all()
 
 # Create your views here.
