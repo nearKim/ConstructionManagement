14cf8186ba32f7e3472e1b85b4927095225fb112
nearKim <garfield@snu.ac.kr>
2019-03-16 11:50:18 +0900

    누락된 basename 추가함

--- informations/urls.py
+++ informations/urls.py
@@ -4,8 +4,8 @@ from informations import views
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
-router.register(r'duration-infos', views.DurationInfoViewSet)
-router.register(r'productivity-infos', views.ProductivityInfoViewSet)
+router.register(r'duration-infos', views.DurationInfoViewSet, basename='duration-info')
+router.register(r'productivity-infos', views.ProductivityInfoViewSet, basename='productivity-info')
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
