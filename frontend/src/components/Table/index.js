bd0384efcee849b0c57ddcd0241afeeed25dd412
nearKim <garfield@snu.ac.kr>
2019-03-30 13:42:08 +0900

    React Bootstrap Table을 렌더링하는 컴포넌트를 작성함

--- frontend/src/components/Table/index.js
+++ frontend/src/components/Table/index.js
@@ -1,45 +1,30 @@
 import React, {Component} from 'react'
 import BootstrapTable from 'react-bootstrap-table-next';
-import {convertData4BootstrapTable} from '../../common/utils'
 
 export default class Table extends Component {
     constructor(props) {
         super(props)
 
-        this.initialized = props.data.length !== 0 ? true : false
+        // Data가 안들어오면 렌더링하지 않는다
+        this.initialized = props.data.length !== 0
 
         if (this.initialized) {
-            this.data = convertData4BootstrapTable(props.data)
-
-            // data는 무조건 넘어오므로 이로부터 key들을 초기화시킨다
-            this.keys = Object.keys(this.data[0])
-
-            // Idfield는 무조건 key에 id가 들어갈 것이다.
-            this.idField = Object.keys(this.data[0]).filter(key => {
+            // Bootstrap table을 위한 데이터들을 가공하여 넣어준다.
+            this.keys = Object.keys(props.data[0])
+            this.columns = this.keys.map(key => ({dataField: key, text: key}))
+            this.keyField = this.keys.filter(key => {
                 return key.includes('id')
             })[0]
         }
 
     }
 
-    // 주어진 data로부터 ReactBootstrapTable이 렌더링할 컬럼 정보를 뽑아낸다
-    filterColumns(data) {
-        let columns = []
-        this.keys.map(key => {
-            columns.push({
-                dataField: key,
-                text: key
-            })
-        })
-        return columns
-    }
-
     render() {
         return (
             this.initialized ?
-                <BootstrapTable keyField={this.idField}
-                                columns={this.filterColumns(this.data)}
-                                data={this.data}/>
+                <BootstrapTable keyField={this.keyField}
+                                columns={this.columns}
+                                data={this.props.data}/>
                 : null
         );
     }
