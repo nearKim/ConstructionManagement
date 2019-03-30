843751e615b038b59ef439d90b4f4e5fedb6faef
nearKim <garfield@snu.ac.kr>
2019-03-30 13:38:42 +0900

    WorkPackage를 분류하는 method를 추가하고 shallow copy 하도록 리팩터함

--- frontend/src/common/utils/index.js
+++ frontend/src/common/utils/index.js
@@ -5,11 +5,42 @@ export function fetch(input, init = {}) {
 }
 
 /**
+ * WorkPackage들의 리스트를 받아서 대소분류 나눠서 반환한다
+ * @param workPackages
+ */
+export function aggregateWorkPackages(workPackages) {
+    // 초기화
+    let aggregatedWP = {}
+
+    // 대분류 추출한다
+    let parentPackages = workPackages.filter(wp => {
+        return wp.parent_package == null
+    })
+
+    workPackages.filter(wp => {
+        return wp.parent_package != null
+    }).map(cp => {
+            // Child Package의 parent package를 찾는다. 1개의 Parent는 1개의 Child를 가지는 것이 보장된다.
+            let pp = parentPackages.find(pp => {
+                return pp.id === cp.parent_package
+            })
+
+            // data에 parent:child 형식으로 넣어준다.
+            aggregatedWP[pp.package_name] = cp.package_name
+        }
+    )
+    return aggregatedWP
+}
+
+/**
  * Input data의 work_package들을 분류하여 BootstrapTable이 이해할 수 있는 형태로 변환한다
  * {parent_package_name: child_package_name } 형식으로 반환해야 한다
  * @param data: api로 부터 받아온 데이터들
  */
-export function convertData4BootstrapTable(dataArr) {
+export function convertData4BootstrapTable(rawDataArr) {
+    // shallow copy
+    let dataArr = rawDataArr.slice()
+
     dataArr.map(data => {
         // created와 updated는 빼줍시다
         delete data.created
@@ -20,25 +51,8 @@ export function convertData4BootstrapTable(dataArr) {
             let workPackages = data.work_package
             delete data.work_package
 
-            // 먼저 Parent Package들을 뽑아낸다
-            let parentPackages = workPackages.filter(wp => {
-                return wp.parent_package == null
-            })
+            data = {...data, ...aggregateWorkPackages(workPackages)}
 
-            // Child Package들을 대상으로 로직을 적용한다.
-            workPackages.filter(wp => {
-                    return wp.parent_package != null
-                }
-            ).map(cp => {
-                    // Child Package의 parent package를 찾는다. 1개의 Parent는 1개의 Child를 가지는 것이 보장된다.
-                    let pp = parentPackages.find(pp => {
-                        return pp.id === cp.parent_package
-                    })
-
-                    // data에 parent:child 형식으로 넣어준다.
-                    data[pp.package_name] = cp.package_name
-                }
-            )
         }
 
         // Activity인 경우 resource를 name으로 바꿔넣어준다
