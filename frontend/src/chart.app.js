943570d56bd82312dc7e495053d8b24520c4719e
nearKim <garfield@snu.ac.kr>
2019-05-06 12:20:42 +0900

    누락된 Component 추가함

--- frontend/src/chart.app.js
+++ frontend/src/chart.app.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import Chart from './scenes/Chart'
 
 
 const root = document.getElementById('root')
 
 if (root) {
     ReactDOM.render(
+        <Chart/>,
         root
     )
 }
--- frontend/src/scenes/Chart/index.js
+++ frontend/src/scenes/Chart/index.js
@@ -1,15 +1,46 @@
 import React, {Component, ReactPropTypes} from 'react';
-import './index.css';
+
+import * as api from '../../common/api';
+import {
+    XYPlot,
+    XAxis,
+    YAxis,
+    VerticalGridLines,
+    HorizontalGridLines,
+    VerticalBarSeries,
+} from 'react-vis';
 
 export default class Chart extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            initialized: false
+            initialized: false,
+            histogramData: null,
+
         }
     }
 
     componentDidMount() {
+        api.getHistogramData()
+            .then(res => res.json())
+            .then(histogramData => {
+                this.setState({
+                    initialized: true,
+                    histogramData
+                })
+            })
+    }
 
+    render() {
+        return (
+            this.state.initialized &&
+            <XYPlot xType="ordinal" width={300} height={300} xDistance={100}>
+                <VerticalGridLines/>
+                <HorizontalGridLines/>
+                <XAxis/>
+                <YAxis/>
+                <VerticalBarSeries data={this.state.histogramData.data}/>
+            </XYPlot>
+        )
     }
 }
