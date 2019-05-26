9bdb3c222fc24bdb6b1f557aa1d6f6a123db10a1
nearKim <garfield@snu.ac.kr>
2019-05-26 14:41:52 +0900

    아주 간단한 스피너 추가함

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -5,9 +5,14 @@ import * as api from "../../common/api"
 import ButtonGroup from "reactstrap/es/ButtonGroup";
 
 export default class NavBar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {initialized: true}
+    }
 
     // Activity를 밀어버린다
     deleteActivities() {
+        this.setState({initialized:false})
         if (confirm('정말 모든 Activity들을 삭제하시겠습니까?')) {
             api.deleteActivities().then(res => {
                 if (res.ok) {
@@ -20,6 +25,7 @@ export default class NavBar extends Component {
 
     // PlannedSchedule을 밀어버린다
     deletePlannedSchedules() {
+        this.setState({initialized:false})
         if (confirm('정말 모든 Planned Schedule들을 싸그리 없애벌이시겠읍니까?')) {
             api.deletePlannedSchedules().then(res => {
                 if (res.ok) {
@@ -32,6 +38,7 @@ export default class NavBar extends Component {
 
     // Allocation을 밀어버린다
     deleteAllocations() {
+        this.setState({initialized:false})
         if (confirm('정말로 Allocation들을 모두 삭제하시겠습니까?')) {
             api.deleteAllocations().then(res => {
                 if (res.ok) {
@@ -44,6 +51,7 @@ export default class NavBar extends Component {
 
     // DB를 밀어버린다
     truncateDatabase() {
+        this.setState({initialized:false})
         if (confirm('정말로 DB를 밀어버리시겠습니까?')) {
             api.truncateDatabase().then(res => {
                 if (res.ok) {
@@ -58,29 +66,37 @@ export default class NavBar extends Component {
 
     render() {
         return (
-            <div>
-                <Navbar color="light" light expand="md">
-                    <NavbarBrand href="/" className="mr-auto">Construction Management</NavbarBrand>
-                    <Nav>
-                        <NavItem>
-                            <NavLink href="/chart/">Visualization</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/planned-schedule/">Planned Schedule management</NavLink>
-                        </NavItem>
-                        <ButtonGroup>
-                            <Button color="warning"
-                                    onClick={() => this.deleteActivities()}>Truncate Activities</Button>
-                            <Button color="warning"
-                                    onClick={() => this.deletePlannedSchedules()}>Truncate Planned Schedules</Button>
-                            <Button color="warning"
-                                    onClick={() => this.deleteAllocations()}>Truncate Allocations</Button>
-                            <Button color="danger"
-                                    onClick={() => this.truncateDatabase()}>Flush DB</Button>
-                        </ButtonGroup>
-                    </Nav>
-                </Navbar>
-            </div>
+            this.state.initialized ?
+                <div>
+                    <Navbar color="light" light expand="md">
+                        <NavbarBrand href="/" className="mr-auto">Construction Management</NavbarBrand>
+                        <Nav>
+                            <NavItem>
+                                <NavLink href="/chart/">Visualization</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/planned-schedule/">Planned Schedule management</NavLink>
+                            </NavItem>
+                            <ButtonGroup>
+                                <Button color="warning"
+                                        onClick={() => this.deleteActivities()}>Truncate Activities</Button>
+                                <Button color="warning"
+                                        onClick={() => this.deletePlannedSchedules()}>Truncate Planned
+                                    Schedules</Button>
+                                <Button color="warning"
+                                        onClick={() => this.deleteAllocations()}>Truncate Allocations</Button>
+                                <Button color="danger"
+                                        onClick={() => this.truncateDatabase()}>Flush DB</Button>
+                            </ButtonGroup>
+                        </Nav>
+                    </Navbar>
+                </div>
+                :
+                <div className="d-flex justify-content-center">
+                    <div className="spinner-border" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                </div>
         )
     }
 }
--- frontend/src/scenes/ConstructionManagement/index.js
+++ frontend/src/scenes/ConstructionManagement/index.js
@@ -458,7 +458,11 @@ export default class ConstructionManagement extends Component {
                                  toggleModalHandler={this.toggleModal}
                     />
                 </div>
-                : null
+                : <div className="d-flex justify-content-center">
+                    <div className="spinner-border" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                </div>
         )
     }
 }
